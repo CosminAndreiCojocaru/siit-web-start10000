ee9da0cc45e18711607b7b336e634a0ce6aec2f5
CosminAndreiCojocaru <95714764+CosminAndreiCojocaru@users.noreply.github.com>
2022-09-05 12:00:59 +0300

    Update OrderProduct.java

--- src/main/java/siit/model/OrderProduct.java
+++ src/main/java/siit/model/OrderProduct.java
@@ -1,75 +1,35 @@
 package siit.model;
 
-import java.util.Locale;
-
 public class OrderProduct {
     private Integer id;
     private Integer orderId;
     private Integer productId;
     private Double quantity;
     private Product product;
-    private String product1;
-
-    public void setValue(Double value) {
-        this.value = value;
-    }
-
     private Double value;
-
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-        if ( product != null ) {
-            setValue(this.quantity * product.getPrice());
-        }
-    }
-
-    public String getProduct1(Locale.Category category) {
-        return product1;
-    }
-
-    public void setProduct1(String product1) {
-        this.product1 = product1;
-    }
-
-    public OrderProduct(Product product1) {
-        this.product1 = String.valueOf(product1);
-    }
-
 
     public OrderProduct(Integer id, Integer orderId, Integer productId, Double quantity) {
         this.id = id;
         this.orderId = orderId;
         this.productId = productId;
         this.quantity = quantity;
-        this.value = 0.0;
+        this.value = 0.0D;
     }
-
-    public OrderProduct() {
-
-    }
-
 
     public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        if ( product != null ) {
-            this.product = product;
-            this.value = getQuantity() * product.getPrice();
-        }
+        return this.product;
     }
 
     public Double getValue() {
-        return value;
+        return this.value;
     }
 
     public Integer getProductId() {
-        return productId;
+        return this.productId;
     }
 
     public Integer getId() {
-        return id;
+        return this.id;
     }
 
     public void setId(Integer id) {
@@ -77,14 +37,30 @@ public class OrderProduct {
     }
 
     public Integer getOrderId() {
-        return orderId;
+        return this.orderId;
     }
 
     public Double getQuantity() {
-        return quantity;
+        return this.quantity;
     }
 
+    public void setValue(Double value) {
+        this.value = value;
+    }
 
-    public void save(OrderProduct p) {
+    public void setProduct(Product product) {
+        if (product != null) {
+            this.product = product;
+            this.value = this.getQuantity() * product.getPrice();
+        }
+
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+        if (this.product != null) {
+            this.setValue(this.quantity * this.product.getPrice());
+        }
+
     }
 }
